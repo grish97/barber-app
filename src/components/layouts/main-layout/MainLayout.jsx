2c93a1a223db95d798974f1d1e0833e9c3ed48a2
grishavasilyan <grisha.vasilyan@volo.global>
2025-11-01 12:21:50 +0400

    first commit

---
+++ src/components/layouts/main-layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import './MainLayout.css';
+
+export const MainLayout = () => {
+    return (
+        <section id='main-layout'>
+            <div id='navbar'>Navbar</div>
+
+            <div id='content'></div>
+        </section>
+    );
+};
